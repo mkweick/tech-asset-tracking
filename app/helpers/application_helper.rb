486392b5a9b51f4c53bbd3e2e555d0c905320adc
Matt Weick <mkweick@gmail.com>
2015-12-07 13:55:24 +0000

    update header title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   
   def format_page_title
-    @page_title.blank? ? "Keller Safety" : "#{@page_title} - Keller Safety"
+    @page_title.blank? ? "Employee Asset Manager" : "#{@page_title} - Employee Asset Manager"
   end
 end
